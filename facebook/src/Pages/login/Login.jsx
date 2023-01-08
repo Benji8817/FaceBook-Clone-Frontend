2343a5347bf22e1af6dd618191a1452288571ebb
LonniR <Lonnir2@gmail.com>
2023-01-07 21:52:27 -0500

    finished loginPg

--- facebook/src/Pages/login/Login.jsx
+++ facebook/src/Pages/login/Login.jsx
@@ -1,24 +1,25 @@
 import React from 'react'
 import "./login.scss"
+// import welcomeimage from "../../assets/pexels-maksim-goncharenok-5821029.jpg"
 
 const Login = ()=>{
     return (
         <div className="login">
             <div className="card">
                 <div className="left">
-                    <h1>Hello World</h1>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur 
-                        sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    {/* <img src={welcomeimage}></img> */}
+                    <h1>Hey You There!!</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor.</p>
                         <span>Don't you have an  account?</span>
                         <button>Register</button>
                 </div>
                     <div className="right">
                         <h1>Login</h1>
-                        <from>
+                        <form>
                             <input type="text" placeholder="Username"></input>
                             <input type="Password" placeholder="Password"></input>
                             <button>Login</button>
-                        </from>
+                        </form>
                     </div>
 
                     
